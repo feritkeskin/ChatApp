47a74f247dd4f8e4de4d8e00ba43d9db4edf99bf
feritkeskin <ferit.keskin69@gmail.com>
2022-10-07 13:11:07 +0300

    update.

--- app/src/main/java/com/feritkeskin/chatapp/activity/DeleteAccountActivity.kt
+++ app/src/main/java/com/feritkeskin/chatapp/activity/DeleteAccountActivity.kt
@@ -25,10 +25,10 @@ class DeleteAccountActivity : AppCompatActivity() {
             val alert = android.app.AlertDialog.Builder(this)
             alert.setTitle("Hesabımı Kalıcı Olarak Sil")
             alert.setMessage("Hesabını Silmek İstediğine Emin Misin?")
-            alert.setPositiveButton("Evet") { dialog, which ->
+            alert.setPositiveButton("EVET") { dialog, which ->
                 deleteAccount()
             }
-            alert.setNegativeButton("Hayır") { dialog, which ->
+            alert.setNegativeButton("HAYIR") { dialog, which ->
                 Toast.makeText(applicationContext, "Hesap Silme Başarısız!!", Toast.LENGTH_SHORT)
                     .show()
             }
