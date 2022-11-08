b782061459da76a7f90a017add2d3d88ca919ce1
feritkeskin <ferit.keskin69@gmail.com>
2022-11-08 22:08:42 +0300

    LoginActivity.kt Update.

--- app/src/main/java/com/feritkeskin/chatapp/activity/LoginActivity.kt
+++ app/src/main/java/com/feritkeskin/chatapp/activity/LoginActivity.kt
@@ -42,6 +42,7 @@ class LoginActivity : AppCompatActivity() {
                             binding.girisParola.setText("")
                             val intent = Intent(this@LoginActivity, UsersActivity::class.java)
                             startActivity(intent)
+                            Toast.makeText(applicationContext, "Hoşgeldin", Toast.LENGTH_SHORT).show()
                             finish()
                         }
                     }
@@ -49,7 +50,6 @@ class LoginActivity : AppCompatActivity() {
                         Toast.makeText(this, "Hata: ${it.message}", Toast.LENGTH_SHORT).show()
                     }
             }
-            Toast.makeText(applicationContext, "Hoşgeldin", Toast.LENGTH_SHORT).show()
         }
 
         binding.girisYeniUyelik.setOnClickListener {
